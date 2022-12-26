23b0df3c5e9a2bc8af0f12ce833604acafe0e79c
João Melo <31737800+jhonas8@users.noreply.github.com>
2022-12-26 15:57:30 -0300

    finishes produtos

--- components/Service.js
+++ components/Service.js
@@ -1,45 +1,86 @@
-import React from 'react';
-import { Container, Row, Col } from "reactstrap";
+import React, { useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const Service = () => {
   const services = [
-    { title : "Lorem Ipsum", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Lorem Ipsum", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Lorem Ipsum", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Lorem Ipsum", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Lorem Ipsum", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Lorem Ipsum", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-  ]
-  
+    {
+      title: 'Bolo de chocolate molhadinho com calda de cereja',
+      icon: 'images/bolo.png',
+      desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    },
+    {
+      title: 'Concha de Colher: Torta Búlgara',
+      icon: 'images/ovo.webp',
+      desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    },
+    {
+      title: 'Panetone de Pistache, Limão e Framboesa',
+      icon: 'images/panetone.jpg',
+      desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    },
+    {
+      title: 'Banana Real',
+      icon: 'images/pastel.webp',
+      desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    },
+    {
+      title: 'Lascas Crocantes de Avelã',
+      icon: 'images/sla.webp',
+      desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    },
+    {
+      title: 'Sonho',
+      icon: 'images/bolosalgado.webp',
+      desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    },
+  ];
+
+  useEffect(() => {
+    const servicesEl = document.querySelectorAll('#service-col');
+
+    const DELAY_TIME_MS = 100;
+
+    servicesEl.forEach((el, index) => {
+      el.setAttribute('data-aos-delay', (index * DELAY_TIME_MS).toString());
+    });
+  }, []);
+
   return (
-    <section className="section" id="service">
+    <section className="section" id="service" data-aos="zoom-in">
       <Container>
         <Row className="justify-content-center">
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
-              <h3 className="font-weight-normal text-dark"><span className="text-warning">Services</span></h3>
-              <p className="text-muted">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+              <h3 className="font-weight-normal text-dark">
+                <span className="text-warning">Produtos</span>
+              </h3>
+              <p className="text-muted">Alguns de nossos produtos já disponíveis ao público.</p>
             </div>
           </Col>
         </Row>
         <Row>
-          {
-            services.map((service, key) =>
-              <Col key={key} lg={4} md={6}>
-                <div>
-                  <div className="mb-5">
-                      <i className={service.icon}></i>
+          {services.map((service, key) => (
+            <Col key={key} lg={4} md={6} id="service-col" data-aos="zoom-in">
+              <div className=" w-35 m-4 position-relative">
+                <div className="card bg-transparent border-none border-0">
+                  <div className="overflow-hidden rounded-circle mb-2">
+                    <img
+                      className="card-img-top object-fit-cover"
+                      height={300}
+                      width={300}
+                      alt="image"
+                      src={service.icon}
+                    ></img>
                   </div>
-                  <h5 className="text-dark font-weight-normal pt-1 mb-4">{service.title}</h5>
-                  <p className="text-muted mb-4">{service.desc}</p>
+                  <p className="text-center font-weight-normal text-capitalize">{service.title}</p>
                 </div>
-              </Col>
-            )
-          }
+              </div>
+            </Col>
+          ))}
         </Row>
       </Container>
     </section>
   );
-}
+};
 
 export default Service;
